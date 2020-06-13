2d2edfbb1c79f49bc12015752d03622dc6734fd0
MEKSankalpa <66584748+MEKSankalpa@users.noreply.github.com>
2020-06-14 00:53:52 +0530

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+     .navcontainer{
+     padding-right:0;
+     padding-left:0;
+     }
+     
+    </style>
+  
+  </head>
+  <body>
+    <div class="container-fluid navcontainer">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark ">
+            <a href="#" class="navbar-brand">SlitCric</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navi"><span class="navbar-toggler-icon"></span></button> 
+            <div class="collapse navbar-collapse navi" >
+             
+            <ul class="nav nav-pills navbar-nav">
+                <li class="nav-item  "><a href="#" class="nav-link active">Home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Matches</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Players</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Contact Us</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
+                
+            </ul>
+            
+            </div>
+            <div class="collapse navbar-collapse justify-content-end navi">
+            <ul class="navbar-nav ">
+                 <li class="nav-item "><a href="#" class="nav-link">Login</a></li>
+                 <li class="nav-item " ><a href="#" class="nav-link">Signin</a></li>
+             </ul> 
+            </div>
+             
+           
+        </nav>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
