c1914814512ae91ee50e9e4a99bd3abc8f50850c
bbsharath52-MCA <66423199+bbsharath52-MCA@users.noreply.github.com>
2020-06-14 14:06:14 +0530

    Create tables.sql

---
+++ tables.sql
@@ -0,0 +1,6 @@
+// table for register and login
+create table 'users' (id int(6), username varchar(20), password varchar(20), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
+
+
+// database
+CREATE DATABASE 'meksankalpa';
