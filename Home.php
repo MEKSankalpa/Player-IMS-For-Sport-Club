385bab666c54259ecffb2db21694fa283b7bc338
bbsharath52-MCA <66423199+bbsharath52-MCA@users.noreply.github.com>
2020-06-14 14:05:48 +0530

    Update Home.php

--- Home.php
+++ Home.php
@@ -5,7 +5,6 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <style>
@@ -25,7 +24,7 @@
             <div class="collapse navbar-collapse navi" >
              
             <ul class=" navbar-nav">
-                <li class="nav-item  "><a href="#" class="nav-link active">Home</a></li>
+                <li class="nav-item  "><a href="http://localhost/git%20hub/Home.php" class="nav-link active">Home</a></li>
                 <li class="nav-item dropdown"><a href="#" class="nav-link dropdown-toggle" type="button" data-toggle="dropdown">Matches</a>
                  <ul class="dropdown-menu bg-light ">
                    <li class="dropdown-item"><a href="" class="dropdown-link ">Up Coming Matches</a></li>
@@ -43,7 +42,7 @@
             </div>
             <div class="collapse navbar-collapse justify-content-end navi">
             <ul class="navbar-nav ">
-                 <li class="nav-item "><a href="#" class="nav-link">Login</a></li>
+                 <li class="nav-item "><a href="http://localhost/git%20hub/login.php" class="nav-link">Login</a></li>
                  <li class="nav-item " ><a href="#" class="nav-link">Sign In</a></li>
              </ul> 
             </div>
@@ -51,11 +50,12 @@
            
         </nav>
     </div>
-
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      
+      <center>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.<?php echo <a href='http://localhost/git%20hub/logout.php'; ?></center>
   </body>
 </html>
